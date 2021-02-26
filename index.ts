6ed59167e9c666c6792ed01c597675b9de5bbb7b
pxpcandy <39356292+pxpcandy@users.noreply.github.com>
2021-02-25 19:21:28 -0600

    init

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 import WebSocketManager from './src/ws/WebSocketManager.ts'
 const ws = new WebSocketManager();
 
-ws.login('ODAxNjQxNDU0NjAyMDI3MDE5.YAjoyA.N4lTIChkEByY6tcew-G4gi_I3W0')
+ws.login('token')
