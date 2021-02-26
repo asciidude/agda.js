647b539518e4250be5eaec1f373af7624aa9e16f
pxpcandy <39356292+pxpcandy@users.noreply.github.com>
2021-02-25 19:21:06 -0600

    init

---
+++ src/constants/Constants.ts
@@ -0,0 +1,17 @@
+export enum Constants {
+    GATEWAY = 'wss://gateway.discord.gg/?v=6&encoding=json',
+}
+
+export enum OPCode {
+    ZERO = 0,
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    ELEVEN = 11
+}
---
+++ src/ws/WebSocketManager.ts
@@ -0,0 +1,58 @@
+import * as websockets from "https://deno.land/std@0.88.0/ws/mod.ts";
+import { Heartbeat, Identify } from '../constants/Payloads.ts';
+import { Constants, OPCode } from '../constants/Constants.ts';
+
+export default class WebSocketManager {
+    private ws: WebSocket | undefined;
+    private interval: any;
+
+    // Connect to WebSocket
+    async login(token: string) {
+        try {
+            // Connect to the gateway
+            try {
+                this.ws = new WebSocket(Constants.GATEWAY);
+            } catch(e) {
+                console.error(new Error("Unable to connect to gateway, detailed error:\n" + e))
+            }
+
+            if(!this.ws) return;
+
+            // Notify the user they connected to the Discord Gateway
+            this.ws.onopen = (() => console.log("Connected to Discord Gateway"));
+
+            this.ws.onmessage = (async(message) => {
+                // Get payload data
+                const payload = JSON.parse(message.data);
+                console.log(payload);
+
+                const { t: event, s, op, d } = payload;
+                const { heartbeatInterval } = d;
+
+                // op codes, woohoo!
+                switch(op) {
+                    // Send hearbeat & identify ourselves
+                    case OPCode.TEN:
+                        this.interval = this.heartbeat(heartbeatInterval);
+                        await this.identify(token);
+                        break;
+                }
+            });
+        } catch (e) {
+            return e;
+        }
+    }
+
+    heartbeat(ms: number) {
+        return setInterval(async() => {
+            if(!this.ws) return; if(this.ws.readyState !== 1) return;
+            await this.ws.send(JSON.stringify(Heartbeat));
+        }, ms);
+    }
+
+    async identify(token: string) {
+        if(!this.ws) return; if(this.ws.readyState !== 1) return;
+        Identify.d.token = token;
+        return this.ws.send(JSON.stringify(Identify));
+    }
+}
