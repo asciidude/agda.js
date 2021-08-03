571a936cc62579eb958196b2aa5e328806f99686
James Barr <sickspamemail@gmail.com>
2021-08-03 12:15:47 -0500

    fix example ping.js

--- examples/ping.js
+++ examples/ping.js
@@ -2,11 +2,7 @@ import Client from '../src/client/Client.js';
 let client = new Client();
 
 import dotenv from 'dotenv';
-import { options } from '../src/ws/WebSocketManager.js';
 dotenv.config();
-
-options.debugMode = true;
-options.debugMode_queue = true;
 
 client.on('ready', async() => {
     await console.log(`${client.user.username} is now ready`);
